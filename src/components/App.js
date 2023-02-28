b9b71ad4aca9b875b6039260b4654e4718c8980d
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-28 17:21:28 +0200

    return filter

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,18 @@ import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectContacts } from 'redux/contacts/selectors';
-// import { selectFilter } from 'redux/filter/selectors';
+import { selectFilter } from 'redux/filter/selectors';
 import { getContacts } from 'redux/operations';
 import { useEffect } from 'react';
 
 export const App = () => {
   const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
-  // const filter = useSelector(selectFilter);
+  const filter = useSelector(selectFilter);
 
-  // const selectedContacts = contacts.filter(contact =>
-  //   contact.name.toLowerCase().includes(filter.toLowerCase())
-  // );
+  const selectedContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   useEffect(() => {
     dispatch(getContacts());
@@ -30,7 +30,7 @@ export const App = () => {
         {contacts.length ? (
           <>
             <Filter title="Find contacts by name" />
-            <ContactsList currentContacts={contacts} />
+            <ContactsList currentContacts={selectedContacts} />
           </>
         ) : (
           <p>Your phonebook is empty</p>
